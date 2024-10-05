2229ce7803f0c5e3346c0f39b8e7a37173d277d6
alanesg <alanesGsilva@gmail.com>
2024-10-04 23:14:26 -0300

    Desenvolvendo o código

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-let nome= "Alanes Gomes"
-console.log("Olá Pessoal! eu me chamo: "+ (nome))
+//Definindo variáveis Necessárias
+let nomeHeroi= "AlanesdaGFT"
+let quantidadeXP = "Experiencia"
+
+
+//Validação do jogo pela idade utilizando perador And.
+    let idade = 26
+      let idadeVerificada = true
+        let resultado = (( idade >=18 && idadeVerificada === true))
+        console.log (idade + " anos."  + " Cadastro feito com sucesso! Vamos Jogar!!")
+
+// Bonificação do Jogo e Introduzindo estrutura de decisão
+// o Herói pode ganhar bonificação de acordo com o que come.
+    let comida = "lasanha"
+    if(comida === "pedra"){
+    console.log("Você perdeu 100 XP")
+}else if(comida === "lasanha"){
+    console.log ("Parabéns! Você ganhou um bônus de 1000 XP!")
+
+} 
+//Introduzindo laços de repetição
+
+
